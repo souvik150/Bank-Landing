d89623586018189d2ff36865760a2d3d4927d21c
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-08-17 18:46:54 +0530

    Adds components folder

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CardDeal = () => {
+  return <div>Cardeal</div>;
+};
+
+export default CardDeal;
---
+++ src/components/FeedbackCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeedbackCard = () => {
+  return <div>Feedback Card</div>;
+};
+
+export default FeedbackCard;
