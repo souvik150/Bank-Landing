5ebdaea57bb5eda0619f1929495cbdac17f5d722
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-08-17 18:55:42 +0530

    Adds all the assets

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,26 @@ const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats Business Billing CardDeal Testimonials CTA Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <CTA />
+          <Footer />
         </div>
       </div>
     </div>
@@ -33,3 +43,9 @@ const App = () => {
 };
 
 export default App;
+
+// VS Code shortcuts learnt along the way :-
+
+//  Ctrl+w To close the files
+//  Ctrl+left arrow to reach the left end of the code
+// Ctrl+right arrow to reach the right end of the code
